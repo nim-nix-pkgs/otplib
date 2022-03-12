dbef3dee3efe32b6bd4bbc8ce4a981ad82142217
hugosenari <hugosenari@gmail.com>
2022-03-12 02:25:56 -0300

    chore: re index otplib

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Easy to use OTP library for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-otplib-master.flake = false;
+  inputs.src-otplib-master.owner = "dimspith";
+  inputs.src-otplib-master.ref   = "refs/heads/master";
+  inputs.src-otplib-master.repo  = "otplib";
+  inputs.src-otplib-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-otplib-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-otplib-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
